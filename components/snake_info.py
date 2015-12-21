33889edb3c9f24ee0b7869d369c1f539fc4cf198
zyks <pazy011@gmail.com>
2015-12-21 18:27:57 +0100

    Add SnakeInfo component

---
+++ components/snake_info.py
@@ -0,0 +1,8 @@
+class SnakeInfo(object):
+
+    def __init__(self, player, is_head, is_tail, next_segment):
+        self.player = player
+        self.is_head = is_head
+        self.is_tail = is_tail
+        self.next_segment = next_segment
+
