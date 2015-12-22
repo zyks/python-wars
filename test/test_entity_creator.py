914808255cda63a9e280ebd762136e0a33d3f9d2
zyks <pazy011@gmail.com>
2015-12-22 23:26:55 +0100

    [#7] Add more tests to create_snake method

--- test/test_entity_creator.py
+++ test/test_entity_creator.py
@@ -1,5 +1,6 @@
 import unittest
-
+from components.graphics import Graphics
+from components.position import Position
 from engine.engine import Engine
 from entity_creator import EntityCreator
 
@@ -8,13 +9,17 @@ class TestEntityCreator(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
         super(TestEntityCreator, self).__init__(*args, **kwargs)
-        self.entity_creator = EntityCreator(Engine())
+        self.engine = Engine()
+        self.engine._entity_components_packer.add('renderable', [Position, Graphics])
+        self.entity_creator = EntityCreator(self.engine)
 
     def test_create_snake_segment(self):
         self.entity_creator.create_snake_segment('img', 0, 0, 0, 1, True, False, None)
         self.assertEqual(len(self.entity_creator._engine._entity_list), 1)
+        self.assertEqual(self.engine.get_entity_by_group('renderable'), self.engine._entity_list)
 
     def test_create_snake(self):
         self.entity_creator.create_snake(1)
         self.assertEqual(len(self.entity_creator._engine._entity_list), 5)
+        self.assertEqual(self.engine.get_entity_by_group('renderable'), self.engine._entity_list)
 
