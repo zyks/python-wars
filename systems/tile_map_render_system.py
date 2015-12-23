3c7966afd9bc192ccc245eebd39d6bd94dde85cc
Krzysztof Drab <krabex93@gmail.com>
2015-12-23 14:06:02 +0100

    [#12] Add TileMapRenderSystem

---
+++ systems/tile_map_render_system.py
@@ -0,0 +1,32 @@
+import pygame
+from components.tile_map import TileMap
+from engine.system import System
+import game_config
+
+
+class TileMapRenderSystem(System):
+    def __init__(self, engine, screen, atlas):
+        self._engine = engine
+        self._screen = screen
+        self._atlas = atlas
+
+    def start(self):
+        pass
+
+    def update(self, time):
+        entities = self._engine.get_entity_by_group("tile_map")
+        for e in entities:
+            tiles = e.get(TileMap).tiles
+            y = 0
+            for row in tiles:
+                x = 0
+                for tile in row:
+                    atlas_area = (game_config.tile_size * tile.value, 0,
+                                  game_config.tile_size, game_config.tile_size)
+                    t = type(game_config.tile_size * tile.value)
+                    self._screen.blit(self._atlas, (x, y), atlas_area)
+                    x += game_config.tile_size
+                y += game_config.tile_size
+
+    def end(self):
+        pass
