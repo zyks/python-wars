e6322f3f16e7180ffc107043e51bcd851d5aed90
zyks <pazy011@gmail.com>
2015-12-26 19:58:12 +0100

    [#6] Rename Player component to PlayerInfo and add number and is_local properties

--- components/player.py
+++
@@ -1,3 +0,0 @@
-class Player(object):
-    def __init__(self):
-        self.snake = []
---
+++ components/player_info.py
@@ -0,0 +1,7 @@
+class PlayerInfo(object):
+
+    def __init__(self, number, is_local):
+        self.snake = []
+        self.number = number
+        self.is_local = is_local
+
