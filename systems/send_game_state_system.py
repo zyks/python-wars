30a140a3306924b566617829b2c3038361c43aae
Krzysztof Drab <krabex93@gmail.com>
2015-12-28 22:05:38 +0100

    [#17] Add time interval between sending game state

--- systems/send_game_state_system.py
+++ systems/send_game_state_system.py
@@ -9,22 +9,28 @@ class SendGameStateSystem(System):
     def __init__(self, engine):
         self._engine = engine
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._counter = 0
+        self._elapsed_time = 0
+        self._sending_interval = 100
 
     def start(self):
         pass
 
     def update(self, time):
-        players = self._engine.get_entity_by_group('player')
-        power_ups = self._engine.get_entity_by_group('power_up')
+        self._elapsed_time += time
 
-        data = {'players': players, 'power-ups': power_ups}
+        if self._elapsed_time >= self._sending_interval:
+            self._elapsed_time -= self._sending_interval
+            players = self._engine.get_entity_by_group('player')
+            power_ups = self._engine.get_entity_by_group('power_up')
 
-        pickled = pickle.dumps(data)
+            data = {'players': players, 'power-ups': power_ups}
 
-        for player in players:
-            player_info = player.get(PlayerInfo)
-            self._socket.sendto(pickled, (player_info.host, player_info.port))
+            pickled = pickle.dumps(data)
+
+            for player in players:
+                player_info = player.get(PlayerInfo)
+                self._socket.sendto(pickled, (player_info.host, player_info.port))
 
     def end(self):
         pass
-
