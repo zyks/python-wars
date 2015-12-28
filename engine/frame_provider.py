1ff53e86f12b078517fd118af7a8cc6afcadf147
Krzysztof Drab <krabex93@gmail.com>
2015-12-28 22:25:26 +0100

    Add posibility to stop FrameProvider

--- engine/frame_provider.py
+++ engine/frame_provider.py
@@ -4,17 +4,22 @@ class FrameProvider(object):
         self._delta = 0.0
         self._actions = []
         self._time_provider = time_provider
+        self._working = False
 
     def add_action(self, action):
         if hasattr(action, '__call__'):
             self._actions.append(action)
 
     def start(self):
+        self._working = True
         self._clock = self._time_provider()
 
-        while True:
+        while self._working:
             self._delta = self._time_provider() - self._clock
             self._clock = self._time_provider()
 
             for action in self._actions:
                 action(self._delta)
+
+    def stop(self):
+        self._working = False
