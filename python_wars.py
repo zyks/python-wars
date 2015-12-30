1eeebaca3db59388c94f00f417c82ee06dc06a86
Krzysztof Drab <krabex93@gmail.com>
2015-12-30 20:17:01 +0100

    [#19] Add arparse for parsing arguments

--- python_wars.py
+++ python_wars.py
@@ -1,5 +1,4 @@
-from enum import Enum
-import random
+import argparse
 import sys
 import threading
 from game_client import GameClient
@@ -8,35 +7,31 @@ from game_server import GameServer
 
 class PythonWars(object):
 
-    def __init__(self, client_mode=False, server_mode=False):
-        self._client_mode = client_mode
-        self._server_mode = server_mode
+    def __init__(self, arguments):
+        self._arguments = arguments
 
-        if self._server_mode:
+        if self._arguments.mode == "server":
             self._server = GameServer()
             # self._server_thread = threading.Thread(target=self._server.run)
-        elif self._client_mode:
-            self._client = GameClient(int(sys.argv[2]))
+        elif self._arguments.mode == "client":
+            self._client = GameClient(self._arguments.port)
 
     def run(self):
-        if self._server_mode:
+        if self._arguments.mode == "server":
             self._server.run()
             self._server._frame_provider.stop()
             # self._server_thread.start()
 
-        elif self._client_mode:
+        if self._arguments.mode == "server":
             self._client.run()
 
         # self._server._frame_provider.stop()
         # self._server_thread.join()
 
-server_mode = False
-client_mode = False
-for arg in sys.argv:
-    if arg == "--server":
-        server_mode = True
-    elif arg == "--client":
-        client_mode = True
 
-g = PythonWars(client_mode, server_mode)
-g.run()
+parser = argparse.ArgumentParser()
+parser.add_argument("mode", help="specify in which mode game should be run", choices=["client", "server"])
+parser.add_argument("--port", type=int, default=40000, help="specify player port")
+parser.add_argument("-p", "--players", type=int, default=2, help="how many players will be in game")
+
+PythonWars(parser.parse_args()).run()
