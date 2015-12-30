f1b9b36bcedbc99d8bb3731b183fa78843e74747
Krzysztof Drab <krabex93@gmail.com>
2015-12-30 16:29:19 +0100

    [#11] Add player states, and min/max snake length to game_config

--- components/player_info.py
+++ components/player_info.py
@@ -1,9 +1,16 @@
+from enum import Enum
+
+
 class PlayerInfo(object):
+    class State(Enum):
+        ALIVE = 0
+        DEAD = 1
+        WIN = 2
 
     def __init__(self, number, address):
         self.snake = []
         self.number = number
         self.address = address
+        self.state = PlayerInfo.State.ALIVE
         # self.host = host
         # self.port = port
-
--- game_config.py
+++ game_config.py
@@ -1,3 +1,5 @@
 screen_size = (800, 608)
 title = "Python Wars"
 tile_size = 32
+snake_minimal_length = 2
+required_length_to_win = 20
